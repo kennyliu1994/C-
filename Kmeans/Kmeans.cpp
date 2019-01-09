2e8dd5708358283b99dd618981aebc825690bd41
Kenny <kennyliuothers@gmail.com>
2019-01-09 18:56:04 +0800

    read file

--- Kmeans/Kmeans.cpp
+++ Kmeans/Kmeans.cpp
@@ -1,9 +1,42 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include "kmeans.h"
+using namespace std;
 
-int main() { 
- cout << "Hello! World!\n"; 
- cout << "哈囉! C++!\n";
-
- return 0; 
+const int dimension = 4;
+//const int numOfIndividuals = 150;
+txtToVector::txtToVector(string inputTxt, vector<string> outputVector[numOfIndividuals])
+{
+    {
+        fstream IrisData;
+        IrisData.open("IrisData.txt", ios::in);
+        if (!IrisData)
+            cout << "File read failed!/n";
+        string line;
+        int lineNumber = 0;
+        while (getline(IrisData, line))
+        {
+            istringstream ss(line);
+            for (int i = 0; i < dimension; i++)
+            {
+                string word;
+                getline(ss, word, ',');
+                outputVector[lineNumber].push_back(word);
+                //cout << outputVector[lineNumber][i] << " ";
+            }
+            lineNumber++;
+            //cout << endl;
+        }
+        //cout << lineNumber << endl;
+        IrisData.close();
+    }
+}
+int main()
+{
+    vector<string> Iris[numOfIndividuals];
+    txtToVector test("IrisData.txt", Iris);
+    return 0;
 }
---
+++ Kmeans/kmeans.h
@@ -0,0 +1,17 @@
+using namespace std;
+const int numOfIndividuals = 150;
+class txtToVector
+{
+  public:
+    txtToVector(string inputTxt, vector<string> outputVector[numOfIndividuals]);
+
+  private:
+};
+class individual
+{
+  public:
+    int inWhichCluster;
+    double disToCluster;
+
+  private:
+};
