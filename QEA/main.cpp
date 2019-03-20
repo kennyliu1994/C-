c1daa0fd83d0787d5f809787d162568ea8f31f6a
Kenny <kennyliuothers@gmail.com>
2019-03-20 16:39:16 +0800

    QEA完成

--- QEA/main.cpp
+++ QEA/main.cpp
@@ -1,12 +1,12 @@
 #include "lib.cpp"
 
-const unsigned int n = 3;           //number of individual
-const unsigned int s = 1;           //sign bit
-const unsigned int pl = 3;          //point left
-const unsigned int pr = 2;          //point right
-unsigned int m = s + pl + pr;       //Q-bit individual 長度
-unsigned int t;                     //目前第幾代
-const unsigned int iteration = 100; //總迭代數
+const unsigned int n = 3;         //number of individual
+const unsigned int s = 1;         //sign bit
+const unsigned int pl = 3;        //point left
+const unsigned int pr = 2;        //point right
+unsigned int m = s + pl + pr;     //Q-bit individual 長度
+unsigned int t;                   //目前第幾代
+const unsigned int iteration = 25; //總迭代數
 
 class individual //Q,P,Pc
 {
@@ -33,29 +33,29 @@ class QEA
             indi[i].fitness = 0.0;
         }
     };
-    void make(class individual inid[n], class individual inid_child[n])
+    void make(class individual parent[n], class individual child[n])
     {
         rand_Kenny rd;
         for (unsigned int i = 0; i < n; i++)
         {
             for (unsigned int j = 0; j < m; j++)
             {
-                if (rd.range_double_1(0, 1.0) < pow(inid[i].beta[j], 2))
-                    inid_child[i].content[j] = 1;
+                if (rd.range_double_1(0, 1.0) < pow(parent[i].beta[j], 2))
+                    child[i].content[j] = 1;
                 else
-                    inid_child[i].content[j] = 0;
+                    child[i].content[j] = 0;
             }
         }
     }
-    void evaluate(class individual inid[n])
+    void evaluate(class individual indi[n])
     {
         for (unsigned int i = 0; i < n; i++)
         {
-            convert binvec2dec(inid[i].content, inid[i].decimal, pl);
-            inid[i].fitness = obj(inid[i].decimal);
+            convert binvec2dec(indi[i].content, indi[i].decimal, pl);
+            indi[i].fitness = obj(indi[i].decimal);
         }
     }
-    void show(class individual indi[n])
+    void show(class individual indi[n], unsigned int t)
     {
         for (unsigned int i = 0; i < n; i++)
         {
@@ -75,86 +75,44 @@ class QEA
             for (unsigned int j = 0; j < m; j++)
                 cout << indi[i].content[j];
             cout << " , decimal = " << indi[i].decimal;
-            cout << " , fitness1 = " << indi[i].fitness << endl;
+            cout << " , fitness = " << indi[i].fitness << endl
+                 << endl;
         }
-    };
-
-  private:
-    double obj(double x)
-    {
-        return 10 + (pow(x, 2) - 10 * cos(2 * M_PI * x));
     }
-};
-double lookup(double x, double b, double fx, double fb)
-{
-    double theta;
-    if ((x == 0) && (b = 1) && (fx < fb))
+    void firstStore(class individual indi[n], class individual B[n])
     {
-        theta = 0.01 * M_PI;
+        for (unsigned int i = 0; i < n; i++) //因為第一迭代，複製一份P(0)到B作為B的初始化
+        {
+            B[i].alpha.assign(indi[i].alpha.begin(), indi[i].alpha.end());
+            B[i].beta.assign(indi[i].beta.begin(), indi[i].beta.end());
+            B[i].content.assign(indi[i].content.begin(), indi[i].content.end());
+            B[i].decimal = indi[i].decimal;
+            B[i].fitness = indi[i].fitness;
+        }
     }
-    else if ((x == 1) && (b = 0) && (fx < fb))
+    void update(class individual child[n], class individual B[n])
     {
-        theta = -0.01 * M_PI;
-    }
-    else
-    {
-        theta = 0;
-    }
-    return theta;
-}
-void update(vector<double> P, vector<double> B, double obj, double Best, individual indi)
-{
-    double theta;
-    for (unsigned int i = 0; i < m; i++)
-    {
-        theta = lookup(P[i], B[i], obj, Best);
-        if (indi.alpha[i] * indi.beta[i] > 0)
-            theta = -theta;
-        indi.alpha[i] = cos(theta) * indi.alpha[i] - sin(theta) * indi.beta[i];
-        indi.beta[i] = sin(theta) * indi.alpha[i] + cos(theta) * indi.beta[i];
-    }
-}
-bool cmp(individual const &a, individual const &b)
-{
-    return a.fitness < b.fitness;
-}
-int main()
-{
-    srand((unsigned)time(NULL));
-    QEA test;
-    individual indi[n];    //n個個體
-    test.initialize(indi); //初始化個體
-    test.make(indi, indi); //觀察Q得到P
-    test.evaluate(indi);   //用P算期望值
-    //test.show(indi);
-    individual B[n];
-    test.initialize(B);
-    for (unsigned int i = 0; i < n; i++)
-    {
-        B[i].content.assign(indi[i].content.begin(), indi[i].content.end()); //因為第一迭代，複製一份P(0)到B作為B的初始化
-        B[i].decimal = indi[i].decimal;
-        B[i].fitness = indi[i].fitness;
-    }
-    //test.show(B);
-    individual indi_child[n]; //P的child，文章中為P(t)
-    test.initialize(indi_child);
-    while (t < iteration)
-    {
-        t++;
-        test.make(indi, indi_child);
-        test.evaluate(indi_child); //test.show(indi_child);
+        double theta;
         for (unsigned int i = 0; i < n; i++)
         {
-            update(indi_child[i].content, B[i].content, indi_child[i].fitness, B[i].fitness, indi[i]); //更新Q
+            for (unsigned int j = 0; j < m; j++)
+            {
+                theta = lookup(child[i].content[j], B[i].content[j], child[i].fitness, B[i].fitness);
+                if (child[i].alpha[j] * child[i].beta[j] > 0)
+                    theta = -theta;
+                child[i].alpha[j] = cos(theta) * child[i].alpha[j] - sin(theta) * child[i].beta[j];
+                child[i].beta[j] = sin(theta) * child[i].alpha[j] + cos(theta) * child[i].beta[j];
+            }
         }
-        //test.show(indi_child);
-        vector<individual> comp; //對整個class的某項參數作為依據排序
+    }
+    void store(class individual child[n], class individual B[n])
+    {
         for (unsigned int i = 0; i < n; i++)
         {
             comp.push_back(B[i]);
-            comp.push_back(indi_child[i]);
+            comp.push_back(child[i]);
         }
-        sort(comp.begin(), comp.end(), cmp);
+        sort(comp.begin(), comp.end(), cmp); //對整個class的某項參數作為依據排序
         /*for (unsigned int i = 0; i < 2*n; i++)
         {
             cout << comp[i].fitness << endl;
@@ -165,12 +123,66 @@ int main()
             B[i].decimal = comp[i].decimal;
             B[i].fitness = comp[i].fitness;
         }
+    }
+
+  private:
+    vector<individual> comp; //class存到vector做比較用
+    double obj(double x)
+    {
+        return 10 + (pow(x, 2) - 10 * cos(2 * M_PI * x));
+    }
+    double lookup(double x, double b, double fx, double fb)
+    {
+        double theta;
+        if ((x == 0) && (b = 1) && (fx < fb))
+        {
+            theta = 0.01 * M_PI;
+        }
+        else if ((x == 1) && (b = 0) && (fx < fb))
+        {
+            theta = -0.01 * M_PI;
+        }
+        else
+        {
+            theta = 0;
+        }
+        return theta;
+    }
+    static bool cmp(individual const &a, individual const &b)
+    {
+        return a.fitness < b.fitness;
+    };
+};
+int main()
+{
+    srand((unsigned)time(NULL));
+    QEA test;
+    individual indi[n];    //n個個體
+    test.initialize(indi); //初始化個體
+    test.make(indi, indi); //觀察Q得到P
+    test.evaluate(indi);   //用P算期望值
+    //test.show(indi, t);    //show Q(0) & P(0)
+    individual B[n];
+    test.initialize(B);
+    test.firstStore(indi, B);
+    //test.show(B, t);          //show B(0)
+    individual indi_child[n]; //P的child，文章中為P(t)
+    while (t < iteration)
+    {
+        t++;
+        test.initialize(indi_child);
+        test.make(indi, indi_child);
+        test.evaluate(indi_child);
+        //test.show(indi_child, t);
+        test.update(indi_child, B);
+        //test.show(indi_child, t);
+        test.store(indi_child, B);
+        test.show(B, t);
         for (unsigned int i = 0; i < n; i++) //小孩變成父母
         {
             indi[i] = indi_child[i];
         }
     }
-    test.show(B);
-    //file.close();
+    //test.show(B, t);
     return 0;
 }
