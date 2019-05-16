aee21e2b5f427b7e6f22a46c1f1b2f9907e99aa3
Kenny <kennyliuothers@gmail.com>
2019-05-16 12:53:08 +0800

    update

--- NSGA2/main.cpp
+++ NSGA2/main.cpp
@@ -1,13 +1,11 @@
 #include "nsga2.h"
-#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    string dataName = argv[1];
+    string testFunName = argv[1];
     unsigned int run = atoi(argv[2]);
     unsigned int iter = atoi(argv[3]);
-    //show dataset info.
-    nsga2 nsga2(dataName, run, iter);
+    nsga2 nsga2(testFunName, run, iter);
     nsga2.exe();
     return 0;
 }
--- NSGA2/nsga2.h
+++ NSGA2/nsga2.h
@@ -1,18 +1,60 @@
 #include <string>
 #include <iostream>
-
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
+#define crossRate 0.9 //crossover rate
+#define mutRate 0.3   //mutation rate
+#define popSize 2     //number of population
+#define objNum 2      //number of multiobective function
+
+class chromosome
+{
+public:
+    vector<double> value;
+    vector<double> Sp;
+    double fitness1;
+    double fitness2;
+    double d; //crowding distance
+    unsigned int np;
+    unsigned int index;
+};
 class nsga2
 {
 private:
-    string dataName;
+    string testFunName;
     unsigned int run;
-    unsigned int iter;
+    unsigned int iteration;
     unsigned int dim;
-    void paraInit();
+    double upperbound;
+    double lowerbound;
+    vector<int> F[2 * popSize];
+    vector<double> tournament;
+    vector<double> bestFit; 
+    vector<chromosome> Rt;
+    vector<chromosome> sortF;
+    chromosome parent[popSize];
+    chromosome child[popSize];
+    void testFunInit();
+    void init();
+    void evaluate(chromosome ch[]);
+    void tourSelection(chromosome parent[], chromosome child[]);
+    void mutation(chromosome ch[]);
+    void main();
+    void fastNondomSort();
+    void calCrowdDis(vector<int> &F);
+    void SCH(chromosome ch[]);
+    void FON(chromosome ch[]);
+    void POL(chromosome ch[]);
+    void KUR(chromosome ch[]);
+    void ZDT1(chromosome ch[]);
+    void ZDT2(chromosome ch[]);
+    void ZDT3(chromosome ch[]);
+    void ZDT4(chromosome ch[]);
+    void ZDT6(chromosome ch[]);
 
 public:
-    nsga2(const string datasetName, const unsigned int run, const unsigned int iteration);
+    nsga2(const string, const unsigned int run, const unsigned int iteration);
     void exe();
 };
