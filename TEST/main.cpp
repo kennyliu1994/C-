e78ee9696d3e06344cc15c87728de0dc91d6971b
kennyliu1994 <kennyliuothers@gmail.com>
2019-05-16 14:38:40 +0800

    test

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,9 +1,10 @@
 #include "iostream"
-#include "../headers/lib.h"
+#include "lib.h"
 using namespace std;
 
 int main()
 {
+    
     rand_Kenny a;
     cout << a.range_int(1, 5) << endl;
     return 0;
